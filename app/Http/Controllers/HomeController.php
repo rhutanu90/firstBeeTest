45dff1d15d43b36c6519e35c7e1f6ff847249f8c
Rares H <rareshutanu@gmail.com>
2015-07-11 21:01:31 +0200

    test4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,14 +17,21 @@ class HomeController extends Controller{
     public function returnHelloWorld(){
         $response = array();
 
+       // die(phpinfo());
         $response['Msg'] = "Hello Robert";
 
-        $server = "tcp:k67fbb5wd8.database.windows.net,1433";
-        $user = "beeboardUser@k67fbb5wd8";
-        $pwd = "Robert$123";
-        $db = "demoBeeBoard_DB";
+        $server = 'tcp:k67fbb5wd8.database.windows.net';
+        $user = 'beeboardUser@k67fbb5wd8';
+        $pwd = 'Robert$123';
+        $db = 'demoBeeBoard_DB';
 
-        
+        try{
+            $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch(Exception $e){
+            $response['MSG2'] = print_r($e);
+        }
 
         return json_encode($response);
     }
